ca10462b18e5c556b3c9d37d7dbf03fcd7c80055
antonio-Leone-Python <antonioleone00@libero.it>
2024-07-19 09:43:01 +0200

    Add files via upload

--- esercizioanalisi.py
+++ esercizioanalisi.py
@@ -19,31 +19,80 @@ def gestione_anomalie(df):
     pagamento_esagerato=df[df["tariffa_mensile"]>500]
     print(pagamento_esagerato)
 
-#creo la funzione per creare un anuova colonna         tempo stimato 15 minuti inizio 14:48    fine 15:07 non funzionante
+#creo la funzione per creare un anuova colonna         tempo stimato 15 minuti inizio 14:48    fine 15:07 13:13 funzionante
 def nuova_colonna(df): 
     df["costo_per_giga"] = df["tariffa_mensile"] / df["dati_consumati"] 
     print(df)
+
+# esploro relazioni tra Età, Durata_Abonnamento, Tariffa_Mensile e la Churn.  tempo stimato 5 minuti 15:15  fine 15:43 funzionante
+def esplora_relazioni(df):
+    esplora = df.groupby("churn").mean()[["eta","durata_abbonamento","tariffa_mensile"]]
+    print(esplora)
+    
+# identifico le possibili correlazioni del dataframe            tempo stimato 10/15 minuti 15:50    fine 14:57 funzionante
+def possibili_relazioni(df):
+    relazioni=df.corr()
+    print(relazioni)
+    
+      
+# converto la colonna churn in formato numerico(0,1)            tempo stimato 15 minuti 16:00        fine 16:54 funzionante
+def conversione(dataframe):
+    dataframe=df["churn"].map({"si":1,"no":0})
+    print(dataframe)
+
+#normalizzo le colonne numeriche                                 tempo stimato 25 minuti 17:15     fine 17:36
+def normalizzazione(df):
+    df['eta'] = (df['eta'] - df['eta'].mean()) / df['eta'].std()
+    df['durata_abbonamento'] = (df['durata_abbonamento'] - df['durata_abbonamento'].mean()) / df['durata_abbonamento'].std()
+    df['tariffa_mensile'] = (df['tariffa_mensile'] - df['tariffa_mensile'].mean()) / df['tariffa_mensile'].std()
+    df['dati_consumati'] = (df['dati_consumati'] - df['dati_consumati'].mean()) / df['dati_consumati'].std()
+    df['servizio_clienti_contattati'] = (df['servizio_clienti_contattati'] - df['servizio_clienti_contattati'].mean()) / df['servizio_clienti_contattati'].std()
     
 
 
 
 
+ #importo il file csv da un altro modulo per la lettura
+df=pd.read_csv("file.csv")
 
- # creo un dizionario
-dizionario = {
-    "id": [1234,456,789,657],
-    "eta": [34,67,90,99],
-    "durata_abbonamento": [34,45,81,None],
-    "tariffa_mensile": [345,500,789,435],
-    "dati_consumati": [7,900,60,56],
-    "servizio_clienti_contattati": [7,54,None,None],
-    "churn":[True,False,True,False]
-}
 
-#creo un dataframe
-df = pd.DataFrame(dizionario) 
+#creo il menu     tempo stimato 10 minuti   data inizio9:23
+print("""menu
+      1. per la descrizione del dataframe
+      2. per eliminare gli spazi vuoti
+      3. per gestire le anomalie
+      4. per creare una nuova colonna con "costo_oer_giga"
+      5. per per esplorare la relazione tra vari dati
+      6. per avere le possibili relazione con il dataframe 
+      7. per convertire la colonna churn in 0 e 1 
+      8. esci
+      """)
+# creo il ciclo gestendo le varie opzioni
+while True:
+    scelta=input("seleziona un numero")
+    if scelta=="1":
+        esamina_distribuzione(df)
+    elif scelta=="2":
+        elimina_spazi_vuoti(df)
+    elif scelta=="3":
+        gestione_anomalie(df)
+    elif scelta=="4":
+        nuova_colonna(df)
+    elif scelta=="5":
+        esplora_relazioni(df)
+    elif scelta=="5":
+        possibili_relazioni(df)
+    elif scelta=="6":
+        conversione(df)
+    elif scelta=="7":
+        break                    
 
-nuova_colonna(df)
+
+
+
+
+
+
 
 
 
